b374c0eb56a3b61d0f3a44475772359d08059207
kspl456 <146167786+kspl456@users.noreply.github.com>
2025-08-01 16:01:05 +0530

    add user selection

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const computerChoice=document.getElementById("computer-choice");
 const userChoice=document.getElementById("user-choice");
 const resultDisplay=document.getElementById("result");
+const possibleChoices=document.querySelectorAll(".button");
+const startButton=document.querySelector(".start-btn");
+let userSelection;
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener("click",(e)=>{
+    userSelection=e.target.id;
+    userChoice.innerHTML=userSelection;
+}))
